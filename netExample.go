18b97a025567e9cd3fe832dd27fef312dadcec6b
Taha Kamali Mehraban <166524733+tkm315@users.noreply.github.com>
2025-02-04 14:59:38 +0330

    uglyEditing

---
+++ netExample.go
@@ -0,0 +1,69 @@
+package main
+
+//
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Page struct {
+	Title string
+
+	Body []byte
+}
+
+func (p *Page) save() error {
+
+	filename := p.Title + ".txt"
+
+	return os.WriteFile(filename, p.Body, 0600)
+
+}
+
+func loadPage(title string) (*Page, error) {
+
+	filename := title + ".txt"
+
+	body, err := os.ReadFile(filename)
+
+	if err != nil {
+
+		return nil, err
+
+	}
+
+	return &Page{Title: title, Body: body}, nil
+
+}
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	p, _ := loadPage(r.URL.Path[len("/view/"):])
+	fmt.Fprintf(w, "<h1>%s</h1> <div>%s</div>", p.Title, p.Body)
+}
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
